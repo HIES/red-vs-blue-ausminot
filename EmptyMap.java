630896ce8141e108d1ca52bba28749214e81764a
ausminot <31025026+ausminot@users.noreply.github.com>
2017-12-07 08:52:56 -0500

    A little confus-ed

---
+++ EmptyMap.java
@@ -0,0 +1,17 @@
+public class EmptyMap
+{
+    private Map<K,V> map;
+    
+    public EmptyMap()
+    {
+        map = new Map<K,V>();
+    }
+    public EmptyMap(K val1, V val2)
+    {
+        map = new Map<val1,val2>();
+    }
+    public map visualize(String region)
+    {
+        
+    }
+}
